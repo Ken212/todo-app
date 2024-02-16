d63a37bd4bb1df5273724765093c4eafc22dad53
Ken Weinberg <kennethweinberg@gmail.com>
2024-02-16 10:35:46 -0300

    Finishing touches, added final lines in functions.py

--- App 1 - ToDo/functions.py
+++ App 1 - ToDo/functions.py
@@ -1,7 +1,5 @@
 FILEPATH = "todos.txt"
 
-# A comment
-
 def get_todos(filepath=FILEPATH):
     """ Read a text file and return the list of
     to-do items.
@@ -10,9 +8,11 @@ def get_todos(filepath=FILEPATH):
         todos_local = file_local.readlines()
     return todos_local
 
-
 def write_todos(todos_arg, filepath=FILEPATH):
     """ Write the to-do items list in the text file."""
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
 
+if __name__ == "__main__":
+    print("Hello")
+    print(get_todos())
